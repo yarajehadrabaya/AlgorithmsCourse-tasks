8dc40fc1362f25198ae89964912c1357a61642b0
yara jehad rabaya 10 <122912660+yarajehadrabaya@users.noreply.github.com>
2023-12-10 00:29:14 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,52 +1,58 @@
 #include <iostream>
+#include <chrono>
 using namespace std;
 
-// Function to calculate the sum of the first n natural numbers using brute force
-long long int sumOfFirstN( long long  int n) {
-    long long int sum = 0;
-    for (int i = 1; i <= n; ++i) {
-        sum += i;
-    }
-    return sum;
-}
 
-int main() {
-   long long int n;
-    cin >> n;
-
-    long long  int r=  sumOfFirstN(n);
-    cout <<r;
-    return 0;
-}
+unsigned long long iterativeFactorial(int n) {
+    if (n < 0) { return 0;}
 
 
-
-
-
-
-
-
-
-//1.2:
-
-/*
-#include <iostream>
-using namespace std;
-
-// Function to calculate the sum of the first n natural numbers using brute force
-long long int sumOfFirstN(long long int n) {
-  long long sum = n * (n + 1) / 2;
-    return sum;
+    unsigned long long finalResult = 1;
+    for (int i = 1; i <= n; ++i) { finalResult *= i;}
+    return finalResult;
+    
     }
 
 
-int main() {
-   long long int n;
-    cin >> n;
+unsigned long long  recursiveFactorial(int n) {
+    if (n < 0) { return 0;}
 
-    long long  int r=  sumOfFirstN(n);
-    cout <<r;
-    return 0;
+
+    if (n == 0 || n == 1) {return 1;}
+
+
+    return n * recursiveFactorial(n - 1);
 }
 
-*/
+
+int main() {
+    chrono::time_point<chrono::system_clock> start_1, end_1 , start_2, end_2;
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+
+     //Iterative methods:
+     start_1 = chrono::system_clock::now();
+     unsigned long long iterativeFactRuslt = iterativeFactorial(num);
+     end_1 = chrono::system_clock::now();
+     chrono::duration<double> totaltime_1 = end_1 - start_1;
+
+
+    cout << "\nFactorial of " << num<< " in Iterative methods  is: " <<iterativeFactRuslt<< endl;
+    cout << "Execution time: " <<  totaltime_1.count() << " seconds" << endl;
+
+
+    //Recursive methods:
+    start_2 =chrono::system_clock::now();
+    unsigned long long recursiveFactRuslt =  recursiveFactorial(num);
+    end_2 =chrono::system_clock::now();
+    chrono::duration<double> totaltime_2 = end_2 - start_2;
+
+
+    cout << "\nFactorial of " << num<< " in Recursive methods  is: " <<recursiveFactRuslt<< endl;
+    cout << "Execution time: " << totaltime_2.count() << " seconds" << endl;
+
+
+    return 0;
+}
